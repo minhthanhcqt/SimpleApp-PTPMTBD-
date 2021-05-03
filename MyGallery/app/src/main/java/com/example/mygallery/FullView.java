1aca7e16f0340569ae227d62dd8e1bd9bd786534
LuciferRos55 <minhchau186@gmail.com>
2021-05-03 13:39:40 +0700

    Top menu hidden and visible

--- MyGallery/app/src/main/java/com/example/mygallery/FullView.java
+++ MyGallery/app/src/main/java/com/example/mygallery/FullView.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -28,15 +29,12 @@ import java.util.List;
 
 public class FullView extends AppCompatActivity  {
 
-
-    private int on = 1;
-    private Button button;
-    private Toolbar topBar;
-    private BottomNavigationView bottomNavigationView;
     private MyFragmentAdapter  myFragmentAdapter;
     private ViewPager viewPager;
     private ArrayList<Fragment> fragments;
     private ArrayList<ItemImage> images;
+    private Button button;
+    private boolean a;
 
 
     @Override
@@ -44,9 +42,26 @@ public class FullView extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fullview);
 
-        topBar = findViewById(R.id.topToolBar);
-        setSupportActionBar(topBar);
+        Toolbar topbar = findViewById(R.id.topNav);
+        setSupportActionBar(topbar);
 
+        button = findViewById(R.id.onButton);
+        a = topbar.isShown();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (a == true)
+                {
+                    a = false;
+                    topbar.setVisibility(View.GONE);
+                }
+                else
+                {
+                    a = true;
+                    topbar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         ImageGallery imageGallery=new ImageGallery();
         images=imageGallery.listImage(getBaseContext());
@@ -93,21 +108,34 @@ public class FullView extends AppCompatActivity  {
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.fullview_top_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.top_edit:
+                Toast.makeText(this, "Edit edit", Toast.LENGTH_SHORT ).show();
+                break;
+            case R.id.top_item2:
+                Toast.makeText(this, "Item2", Toast.LENGTH_SHORT ).show();
+                break;
+            case R.id.top_item3:
+                Toast.makeText(this, "Item3", Toast.LENGTH_SHORT ).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     boolean isImage(String path)
     {
         String mimeType= URLConnection.guessContentTypeFromName(path);
         return mimeType!=null &&mimeType.startsWith("image");
     }
 
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.top_edit)
-        {
-            Toast.makeText(getApplicationContext(),"Clicked edit", Toast.LENGTH_SHORT).show();
-        }
-        return true;
-    }
 }
